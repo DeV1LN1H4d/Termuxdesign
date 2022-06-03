60ce4f23939369ea14182c03a5595884671343ea
DEVIL NIHAD <88210650+DeV1LN1H4d@users.noreply.github.com>
2022-06-03 16:53:32 +0600

    Update Termuxdesign.py

--- Termuxdesign.py
+++ Termuxdesign.py
@@ -22,7 +22,7 @@ import os,sys,time,random
 print("")
 print("")
 color = ["\\033[1;31m","\\033[1;32m"]
-m = random.choice(color)+"HACKING IS NOT CRIME IT'S A GAME AGAINST OF THE SYSTEM {} \\n"
+m = random.choice(color)+"HACKING IS NOT CRIME IT'S A GAME AGAINST OF THE SYSTEM\\n"
 for msg in m:
     sys.stdout.write(msg)
     sys.stdout.flush()
