36f3d668bb5e7aac1fbab8553880a8564af629fc
DEVIL NIHAD <88210650+DeV1LN1H4d@users.noreply.github.com>
2022-07-19 19:04:50 +0600

    Add files via upload

---
+++ style.sh
@@ -0,0 +1,385 @@
+clear
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo"" 
+echo""
+echo""
+echo""
+echo""
+echo  ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'|lolcat
+echo""
+echo""
+echo""
+echo""
+echo "               "/█1%...................../|lolcat
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'|lolcat
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/█4%....................../|lolcat
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'|lolcat
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/█8%......................./|lolcat
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'|lolcat
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/██10%....................../|lolcat
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'|lolcat
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/██12%....................../|lolcat
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'|lolcat
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/███15%..................../|lolcat
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'|lolcat
+echo " "
+echo " "
+echo " "
+echo " "
+echo "               "/█████19%..................../|lolcat
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'|lolcat
+echo " "
+echo " "
+echo " "
+echo " "
+echo "               "/██████28%..................../|lolcat
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'|lolcat
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/████████30%................../|lolcat
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'|lolcat
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/██████████40%................/|lolcat
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'|lolcat
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/█████████████50%............./|lolcat
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'|lolcat
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/████████████████60%........../|lolcat
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'|lolcat
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/███████████████████88%......./|lolcat
+sleep 2.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo '
+██▄   ▄███▄      ▄   ▄█ █     
+█  █  █▀   ▀      █  ██ █     
+█   █ ██▄▄   █     █ ██ █     
+█  █  █▄   ▄▀ █    █ ▐█ ███▄  
+███▀  ▀███▀    █  █   ▐     ▀ 
+                █▐            
+                ▐             
+'|lolcat
+
+echo ""
+echo "" 
+echo "               "[██████████████████████ 100%]|lolcat
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sleep 1.0
